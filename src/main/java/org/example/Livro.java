78b7871c4317858c0a5b3aafa4687184f11dcbeb
lougans meneses <lougans_meneses@hotmail.com>
2025-07-31 17:05:34 -0300

    Primeiro commit

---
+++ src/main/java/org/example/Livro.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public void exibirInformacoes(){
+        System.out.println("Titulo: " + this.titulo);
+        System.out.println("Autor: " + this.autor);
+        System.out.println("Ano de Publicação: " + this.anoPublicacao);
+        System.out.println("-----------------------------");
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro("Dom Casmurro", "Machado de Assis", 1899);
+        Livro livro2 = new Livro("1984", "George Orwell", 1949);
+
+        livro1.exibirInformacoes();
+        livro2.exibirInformacoes();
+    }
+}
